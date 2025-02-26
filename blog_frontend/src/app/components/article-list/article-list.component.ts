79928cdcd00233037c3a50e78aa5c337e8769d75
Druid45ra <radu_vanca@live.com>
2025-02-26 18:08:39 +0200

    Add article list component and header navigation

---
+++ blog_frontend/src/app/components/article-list/article-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ArticleService } from '../../services/article.service';
+
+@Component({
+  selector: 'app-article-list',
+  templateUrl: './article-list.component.html',
+  styleUrls: ['./article-list.component.css']
+})
+export class ArticleListComponent implements OnInit {
+  articles: any[] = [];
+
+  constructor(private articleService: ArticleService) { }
+
+  ngOnInit(): void {
+    this.articleService.getArticles().subscribe(data => {
+      this.articles = data;
+    });
+  }
+}
